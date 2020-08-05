2976302f580015a62858ca115fbfa81d768f00c3
冰子 <54745251+BingZi-233@users.noreply.github.com>
2020-08-05 15:48:12 +0800

    冰子 - 对GUI进行了保护操作，晚上会进行简化步骤

--- src/main/java/vip/bingzi/randomentry/RandomEntry.java
+++ src/main/java/vip/bingzi/randomentry/RandomEntry.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import vip.bingzi.randomentry.util.RandomEntryCommand;
+import vip.bingzi.randomentry.util.RandomEntryInv;
 
 import java.io.File;
 
@@ -37,6 +38,7 @@ public final class RandomEntry extends JavaPlugin {
     public void onEnable() {
         long startTime = System.currentTimeMillis();
         PluginMain = this;
+        Bukkit.getPluginManager().registerEvents(new RandomEntryInv(),this);
         Bukkit.getPluginCommand("RandomEntry").setExecutor(new RandomEntryCommand());
         long endTime = System.currentTimeMillis();
         if (Debug) getLogger().info("程序执行启动耗时 "+(endTime-startTime)+" 毫秒");
---
+++ src/main/java/vip/bingzi/randomentry/util/RandomEntryInv.java
@@ -0,0 +1,35 @@
+package vip.bingzi.randomentry.util;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import vip.bingzi.randomentry.RandomEntry;
+
+public class RandomEntryInv implements Listener {
+    private static boolean OpenInfo = false;
+    @EventHandler
+    public void onOpenGUI(InventoryOpenEvent event){
+        if (event.getView().getTitle().equalsIgnoreCase(RandomEntry.getPluginMain().getConfig().getString("ViewTitle"))){
+            if (RandomEntry.Debug) RandomEntry.getPluginMain().getLogger().info("检测到打开了鉴定GUI界面，状态已经设置为true");
+            OpenInfo = true;
+        }
+    }
+    @EventHandler
+    public void onCloseGUI(InventoryCloseEvent event){
+        if (event.getView().getTitle().equalsIgnoreCase(RandomEntry.getPluginMain().getConfig().getString("ViewTitle"))){
+            if (RandomEntry.Debug) RandomEntry.getPluginMain().getLogger().info("检测到关闭了鉴定GUI界面，状态已经设置为false");
+            OpenInfo = false;
+        }
+    }
+    @EventHandler
+    public void onPlayerOpenGui(InventoryClickEvent event){
+        // 检查是不是鉴定界面
+        if (event.getView().getTitle().equalsIgnoreCase(RandomEntry.getPluginMain().getConfig().getString("ViewTitle"))){
+            if (RandomEntry.Debug) RandomEntry.getPluginMain().getLogger().info("玩家"+event.getView().getPlayer().getName()+"点击了鉴定界面");
+            event.setCancelled(true);
+            if (RandomEntry.Debug) RandomEntry.getPluginMain().getLogger().info("已经取消点击");
+        }
+    }
+}
